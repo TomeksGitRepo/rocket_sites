f715f6917254e7ed57d6232ad2da419ed3ae63e5
Tomek <tomek7sa@gmail.com>
2017-12-11 10:00:37 +0100

    Working hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+
+mod other {
+    #[get("/world")]
+    pub fn world() -> &'static str {
+        "Hello, world!"
+    }
+}
+
+use other::world;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/hello", routes![other::world]).launch();
 }
